613de8a479d9680b9427d54ecd534acad72e25ee
smileunknownfawn-sketch <smile.unknownfawn@gmail.com>
2026-09-26 10:41:38 +0300

    Add package import smoke tests

---
+++ tests/test_package_imports.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+def test_core_package_imports():
+    import core.database
+    import core.config
+
+
+def test_services_package_imports():
+    import services.storage_service
+    import services.statistics_service
